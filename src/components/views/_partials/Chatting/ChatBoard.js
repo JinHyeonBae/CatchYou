33df88d4427dcd7c58a3e3dc6381511c3897e68f
BJH <bqwert36@hanmail.net>
2020-11-11 01:52:41 +0900

    get chatting data using usequery and add topic Information using data

--- src/components/views/_partials/Chatting/ChatBoard.js
+++ src/components/views/_partials/Chatting/ChatBoard.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React, { createElement, useRef, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import SendChat from "./SendChat"
 import "./chatContainer.scss";
 import { GET_CHAT } from "./gqls";
 import { useQuery } from "@apollo/client";
+import { InMemoryCache } from "@apollo/react-hooks";
+import { gql } from "@apollo/client";
 
 
+function ChatBoard() {
+    const { loading, error, data } = useQuery(GET_CHAT,{ //promise
+        onCompleted(){
+            if(data){
+               const {topic} = data;
+               const {topicDes, topicName, topicType} = topic;
+               
+               topicTitle = topicName;
+               const topicAcessType = document.createElement('p');
+               const topicDescription = document.createElement('p');
+               
+               topicAcessType.innerHTML = topicType;
+               topicDescription.innerHTML = topicDes;
+               titleRef.current.appendChild(topicAcessType);
+               titleRef.current.appendChild(topicDescription);
+            }
+        }
+    });
+    console.log("loading :", loading);
+    console.log("error :", error);
+    
+    const titleRef = useRef();
+    let topicTitle;
 
-function ChatBoard(props) {
-
-    const { loading, error, data } = useQuery(GET_CHAT);
-    //error occurs
-    if (loading) { alert("로딩 중.."); }
-    if (error) { alert("에러 발생"); }
-
-    //const { chats } = data;
-    let isAcceptChat;
-
-    if (data != undefined) {
-        const { chats } = data;
-        isAcceptChat = chats.map(({ id, content }) => (
-            <li key={id}>{content}</li>));
-    }
-
-    const toolbarTitle = "공지사항"
     return (
         <div className="content-side col-lg-9 col-md-12 col-sm-12">
-            <div className="sec-title">
-            <button>즐겨찾기</button>
-                <div title={toolbarTitle}>
-                    <h4>{toolbarTitle}</h4>
+            <div className="sec-title" ref={titleRef}>
+                <button>즐겨찾기</button>
+                <div title={topicTitle}>
+                    <h4>{topicTitle}</h4>
                 </div>
                 <button>토픽설명</button>
             </div> {/*top*/}
@@ -39,7 +47,6 @@ function ChatBoard(props) {
 
             <div className="featured-section">
                 <div className="feature-block-two col-lg-6 col-md-12 col-sm-12">
-                    {isAcceptChat ? isAcceptChat : ''}
                     <SendChat />
                 </div>
             </div> {/*bottom*/}

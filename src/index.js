78f24829542c848375cc0a801a9a733167c0c282
BJH <bqwert36@hanmail.net>
2020-11-11 01:48:53 +0900

    change proxy target and uri

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider, withApollo } from "@apollo/react-hooks"
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from '@apollo/client';            // 수정
+import { ApolloClient } from '@apollo/client';
+import gql from "graphql-tag"         // 수정
+
 
 const httpLink = createHttpLink({                       // 수정
-  uri: 'http://localhost:3000'                          // 수정
+  uri: 'http://localhost:4000/graphql'                          // 수정
 })                                                      // 수정
 //httpLink 옵션 
 
@@ -16,7 +18,8 @@ const httpLink = createHttpLink({                       // 수정
 const client = new ApolloClient({
   link: httpLink,                                       // 수정
   cache: new InMemoryCache()                            // 수정
-}) //httpLink => graphql api를 사용해 apolloClient 인스턴스에 연결
+}); 
+
 
 ReactDOM.render(
   // StrictMode는 잠재적 문제를 식별해서 경고를 제공해주는 역할을 하고 있다.

3b3d491398d7acedb6dc1d35b4ed9f5d6cb32754
JH <bqwert36@hanmail.net>
2021-10-31 20:44:38 +0900

    dto 구성 변경 중

--- server/classes/DTO.py
+++ server/classes/DTO.py
@@ -1,19 +1,26 @@
+import sys
+sys.path.append('C:\\Users\\BJH\\Desktop\\CatchYou\\server')
+
 from queue import Queue
-from Send import Send
+from globalValue import rd
+
+import json, traceback, time
 
 
+
+# productor 클래스
 class DTO:
 
     def __init__(self):
         self.user_env = {
-            "width" : 480,
-            "height" : 720,
+            "width" : 0,
+            "height" : 0,
             "vertical" : 0,
             "horizontal" : 0
         }
-
+    
         self.dataQueue = Queue()
-        self.send = Send()
+
 
     # 데이터를 받아와서 집어넣는다
     # def put(self, data):
@@ -23,17 +30,35 @@ class DTO:
     def calc_screen(self):
         pass
     
-    def put(self, value):
-        self.dataQueue.put(value)
+    def update(self):
+        try :
+            time.sleep(0.1)
+            print("update function")
+            print(self.dataQueue.qsize())
+            return self.dataQueue.get()
+        except Exception :
+            print(traceback.format_exc())
+            print("현재 queue에 아무것도 들어있지 않습니다.")
+            
+    
+    def put(self):
+        self.dataQueue.put_nowait(self.user_env)
 
-    def take(self):
-        #caliData의 type은 아마 json
-        caliData = self.dataQueue.get() 
-        print("caliData type :", type(caliData))
+    def dto_Process(self, value):
+        try:
+            print("dto_process value :", value)
+            caliData = json.loads(value)
+            print("calidate Json :", caliData)
+            print("calidate type :", type(caliData))
+            # calc_screen 이후
+            #self.set_screen(caliData['width'], caliData['height']) 
+            self.set_screen(480, 720) 
+            self.set_pupil_ratio(caliData['vertical'], caliData['horizontal'])
 
-        # calc_screen 이후
-        self.set_screen(caliData['width'], caliData['height']) 
-        self.set_pupil_ratio(caliData['vertical'], caliData['horizontal'])
+            return self.user_env
+            
+        except Exception as e:
+            print(traceback.format_exc())
 
     def set_screen(self, width, height):
         user_info = self.user_env
@@ -48,8 +73,13 @@ class DTO:
         user_info['vertical']   = ver
         user_info['horizontal'] = hor
 
-    def giveBack(self):
+
+    def get(self):
         return self.user_env
+
+    # def giveBack(self):
+    #     return self.user_env
 
     # def send(self):
     #     self.Send.
+

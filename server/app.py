fb1f443515fabf2764309596046de9325823d64e
JH <bqwert36@hanmail.net>
2021-11-05 01:10:55 +0900

    클라이언트로 데이터 전송

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from queue import Empty
-from flask import Flask,jsonify
-from flask_sse import sse
+from flask import Flask,jsonify, make_response, g
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS
 from multiprocessing import Queue
@@ -11,14 +10,15 @@ import logging, datetime, random, flask, sys
 
 from classes.socketServer import socketServer
 from classes.DTO import DTO
-from globalValue import rd
+from classes.producer import Producer
+from classes.consumer import Consumer
 
 
 app = Flask(__name__)
 CORS(app)
 
 # redis의 구성
-app.config["REDIS_URL"] = "redis://redis"
+app.config["REDIS_URL"] = "redis://localhost"
 log = logging.getLogger('apscheduler.executors.default')
 log.setLevel(logging.INFO)
 fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
@@ -27,58 +27,23 @@ h.setFormatter(fmt)
 log.addHandler(h)
 
 
-# background
-
-class factory:
-
-    socQueue = Queue()
-    dto = DTO()
-
-    def productor(self):
-        socServer = socketServer()
-        socServer.listen()
-        print("Wating for clientSocket. current status : listen")
-        socServer.accept()
-
-        while True:
-            socServer.receive()
-            data = socServer.get()
-            # 여기서는 쌓아두고
-            caliData = self.dto.dto_Process(data)
-            
-            self.socQueue.put(caliData)
-            
-            socServer.send()
-
-    def consumer(self):
-        
-        data = []
-        while not self.socQueue.empty():
-            print("consumer herhe")
-            time.sleep(1)
-            caliData = self.socQueue.get()
-            data.append(caliData)
-
-        return data
-
-    
-f = factory()
 
 @app.route('/')
 def stream():
-    print("localhost:5000/ enter")
-    print("큐의 사이즈 : ", f.socQueue.qsize())
-    return "hello"
+    data = consumer.run()
+    print("run data :", data)
+    return jsonify(data)
+
 
 
 if __name__ == '__main__':
     try:
-        # 생산자 스레드
-        prodThread = threading.Thread(target=f.productor)
-        prodThread.start()
-
-        consumerThread = threading.Thread(target=f.consumer)
-        consumerThread.start()
+        socket = socketServer()
+        producer = Producer()
+        consumer = Consumer()
+        
+        socketThread = threading.Thread(target=socket.socketConnect)
+        socketThread.start()
 
         app.run(debug=True,host='localhost',port=5000)
         

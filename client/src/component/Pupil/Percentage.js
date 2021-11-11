08aecd092a2a2bd080fc74e51002a3c911a9145e
JH <bqwert36@hanmail.net>
2021-11-11 20:34:56 +0900

    부정행위 발생 비율이 하나의 컴포넌트로 출력되도록 수정

--- client/src/component/Pupil/Percentage.js
+++ client/src/component/Pupil/Percentage.js
@@ -1,18 +1,26 @@
-
-
-
-
+import React, {useEffect, useState, useRef, Fragment} from 'react'
 
 
 export default function Percentage(props){
 
-    console.log("props :", props.percent)
-    const { cheat_percentage    } = props.percent
-    console.log(cheat_percentage)
+    const [ch_per, set_ch_per] = useState(0)
+    const [index, set_index] = useState(0)
+    const {percent} = props
+
+    useEffect(()=>{
+        console.log(percent.length)
+        set_index(percent.length-1)
+        console.log(percent)
+        console.log("length :" , index)
+        console.log(percent[index].cheat_percentage)
+
+        set_ch_per(percent[index].cheat_percentage)
+    })
 
     return(
         <div>
-            {cheat_percentage}
+            사용자님의 부정행위 확률은
+            {ch_per}
         </div>
 
     )

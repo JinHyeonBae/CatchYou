ec2cc9b25996bd3df88124a02265ce38890acae5
JH <bqwert36@hanmail.net>
2021-11-14 03:33:53 +0900

    소켓과 dto를 연결하는 클래스 생성

--- server/classes/socketServer.py
+++ server/classes/socketServer.py
@@ -2,7 +2,13 @@ from socket import *
 from queue import Queue
 
 import json
+import sys, time
 
+sys.path.append('C:\\Users\\BJH\\Desktop\\CatchYou\\server')
+
+
+from classes.redisQueue import RedisQueue
+from classes.cheating_detect import Pupil, Sound
 
 class socketServer:
 
@@ -24,11 +30,6 @@ class socketServer:
 
     def receive(self):
         data = self.connectionSock.recv(1024)
-        print('받은 데이터 : ', data.decode('utf-8'))
-        print("받은 데이터 타입 :", type(data.decode('utf-8')))
-        print('받은 데이터, no decode :', data)
-        print('받은 데이터, no decode :', type(data))
-
         self.receive_data = data.decode('utf-8')
     
     def send(self):
@@ -39,18 +40,36 @@ class socketServer:
         return self.receive_data
 
 
-# def socketConnect():
-#     print("hihi")
-#     socServer = socketServer()
-#     socServer.listen()
-#     print("listen")
-#     socServer.accept()
+class connect_socket_to_dto:
 
-#     while True:
-#         socServer.receive()
-#         data = socServer.get()
-#         # 여기서는 쌓아두고
-#         dto.dto_Process(data)
-#         dto.put()
-        
-#         socServer.send()
+    def __init__(self):
+        self.p = Pupil()
+        self.s = Sound()
+        self.socServer = socketServer()
+
+    def run(self):
+        self.socServer.listen()
+        print("status : listen")
+        self.socServer.accept()
+
+        while True:
+            time.sleep(5)
+            self.socServer.receive()
+            socket_data = self.socServer.get()
+            # 여기서는 쌓아두고
+            
+            loads_data = self.parse(socket_data)
+            
+            if(loads_data['type'] == 0):
+                self.p.pupil_dto_Process(loads_data)
+
+
+            self.socServer.send()
+
+
+    def parse(self, socket_data):
+
+        print("dto_process value : ",socket_data)
+        loads_data = json.loads(socket_data)
+
+        return loads_data

1df2ad201a96523c16b5d2eac754bf5c9dc63123
JH <bqwert36@hanmail.net>
2021-11-15 22:53:11 +0900

    시선 위치 데이터 출력 방식 변경

--- client/src/component/Pupil/Draw.js
+++ client/src/component/Pupil/Draw.js
@@ -5,15 +5,24 @@ import './points.css'
 
 export default function DrawPoints(props) {
 
-    const {left_x, left_y, right_x, right_y} = props.ur_data
- 
+    console.log(props)
+    const { ur_data  } = props
+    console.log(ur_data)
 
     return(
         <div className="points">
-            초록색 : 왼쪽 눈, 보라색 : 오른쪽 눈
+             <div className="left_point"></div> : 왼쪽 눈 <div className="right_point"></div> : 오른쪽 눈
             <div className="points_canvas">
-                <div className="left_point" style={{'left' : left_x, 'top' : left_y}}></div>
-                <div className="right_point" style={{'left' : right_x, 'top' : right_y}}></div>
+                {ur_data.map((ur, index)=>{
+                    console.log(ur)
+                    if(index == 0) return;
+                    return( 
+                        <Fragment>
+                            <div className="left_point" style={{'left' : ur.left_x, 'top' : ur.left_y}}></div>
+                            <div className="right_point" style={{'left' : ur.right_x, 'top' : ur.right_y}}></div>
+                        </Fragment>
+                    )
+                })}
             </div>
         </div>
     )

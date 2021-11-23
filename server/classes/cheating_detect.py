275080bb96c14e2f5dfb75f051036b59cddd7bb2
JH <bqwert36@hanmail.net>
2021-11-23 22:20:17 +0900

    MessageQueue 구현 방식을 pubsub에서 redisQueue를 사용하는 것으로 변경

--- server/classes/cheating_detect.py
+++ server/classes/cheating_detect.py
@@ -2,153 +2,146 @@
 
 import traceback, json, ast
 #from classes.DTO import DTO
-from classes.redisQueue import RedisQueue,Pubsub
+from classes.redisQueue import RedisQueue
 import random
+import time
 # 
 
-class Pupil():
+class CheatingSystem:
+
+    def __init__(self,name):
+        self.name = name
+
+
+class Pupil:
 
     def __init__(self):
-        super().__init__()
-        self.pubsub = Pubsub('pupil')
-
+        self.pupil_q = RedisQueue(name='pupil')
         self.data_count = 0
         self.true_count = 0
-        self.pupil_data = {
-            "width" : 0,
-            "height" : 0,
-            "left_x" : 0,
-            "left_y" : 0,
-            "right_x" : 0,
-            "right_y" : 0,
-            "cheat_percentage" : 0
-        }
-        self.obj = {
-            'left_x': 0, 
-            'left_y': 0, 
-            'right_x' : 0,
-            'right_y' : 0,
-            'cheat' : 0,
-            'type' : 0
-        }
 
-    # 여기서 데이터를 받아와서 화면의 크기 비율을 계산한다
-    def calc_screen(self):
-        pass
-    
-    
-    def pupil_dto_Process(self, pupil_value : dict):
+    def counting(self):
+        self.data_count += 1
+
+    def update(self, data) -> None : 
+        self.pupil_q.put(data)
+
+
+    def isCheating(self, data : dict):
+        if data['cheat'] :
+            self.true_count +=1
+
+
+    def calculate(self, data):
         try:
-            print("pupil_value : ",pupil_value)
-            # calc_screen 이후
-            #self.set_screen(caliData['width'], caliData['height']) 
-            self.set_screen(480, 720)
-            # self.set_pupil_ratio(pupil_value["left_x"], pupil_value["left_y"], pupil_value["right_x"], pupil_value["right_y"])
+            cheat_percentage = self.true_count / self.data_count
+            data['cheat_percentage'] = cheat_percentage
+            return data
+        except ZeroDivisionError as e:
+            print(e)
+            pass
 
-            # self.check_cheating(pupil_value["cheat"])
-            # self.count()
-            # self.set_cheating_ratio()
-            # self.put()
-            self.obj.update(pupil_value)
-            print("self.obj :", self.obj)
+    def pupil_get(self):
+        for element in self.yield_data():
+            print("pupil get :", element)
 
-        except Exception as e:
-            print(traceback.format_exc())
-
-    def set_screen(self, width, height):
-        user_info = self.pupil_data
-        
-        user_info['width'] = width
-        user_info['height'] = height
-        
-    def set_pupil_ratio(self, l_x, l_y, r_x, r_y):
-        user_info = self.pupil_data
-
-        user_info['left_x'] = l_x
-        user_info['left_y'] = l_y
-        user_info['right_x'] = r_x
-        user_info['right_y'] = r_y
-
-    def set_cheating_ratio(self):
-        user_info = self.pupil_data
-        
-        true_pcent = self.true_count / self.data_count
-        #print("true_pcent :", true_pcent)
-        user_info['cheat_percentage'] = round(true_pcent, 2)
-        #print("cheat_percentage :", user_info['cheat_percentage'])
-        
-    # data count
-    def count(self):
-        self.data_count+=1
-
-    def check_cheating(self, isCheating):
-        print("isCheating :", isCheating)
-        if isCheating:
-            self.true_count+=1
-
-    def put(self):
-        element_str = json.dumps(self.pupil_data)
-        self.pubsub.publish('pupil', element_str)
-
-    def get(self):
-        # gen = self.pubsub.listen()
-
-        # try:
-        #     for pub_data in gen:
-        #         #pub_data = ast.literal_eval(pub_data.decode('utf-8'))
-        #         return pub_data
-        # except AttributeError as e:
-        #     print(traceback.format_exc())
-        #     pass
-
-        print("self.obj in get :", self.obj)
-        return self.obj
+            return element
 
 
-class Sound():
+    def yield_data(self):
+        while self.pupil_q.isEmpty() is False :
+            print(self.pupil_q.size())
+            element = self.pupil_q.get()
+            
+            yield element
+
+
+class Sound:
 
     def __init__(self):
-        super().__init__()
+        self.sound_q = RedisQueue(name='sound')
 
-        self.pubsub = Pubsub('sound')
-        self.sound_data = {
-            "success": True,
-            "error": None,
-            "transcription": None,
-            "cheating_word" : []            
-        }
 
-    def sound_dto_Process(self, sound_value : dict):
-        sound = self.sound_data
-        print("sound_value :", type(sound_value))
-        try:
-            sound['success'] = sound_value['success']
-            sound['error'] = sound_value['error']
-            sound['transcription'] = sound_value['transcription']
-            sound['cheating_word'] = (sound_value['cheating_word'])
-            
-            self.put()
+    def update(self, data : str) -> None :
+        self.sound_q.put(data)
 
-        except Exception :
-            print(traceback.format_exc())
 
-    def put(self):
-        element_str = json.dumps(self.sound_data)
-        print("sound element :", element_str)
-        self.pubsub.publish('sound', element_str)
+    def sound_get(self):
+        for element in self.yield_data():
+            print("pupil get :", element)
+            return element
 
-    def get(self):
-        gen = self.pubsub.listen()
 
-        try:
-            for pub_data in gen:
-                #pub_data = ast.literal_eval(pub_data.decode('utf-8'))
-                print("sound_pub :", pub_data)
-                return pub_data
-        except AttributeError as e:
-            print(traceback.format_exc())
-            pass
+    def yield_data(self):
+        while self.sound_q.isEmpty() is False :
+            time.sleep(3)
+            element = self.sound_q.get()
+            yield element
+
+
+class Person:
+
+    def __init__(self):
+        self.person_q = RedisQueue(name='person')
+
+
+    def update(self, data : str) -> None :
+        self.person_q.put(data)
+
+
+    def person_get(self):
+        for element in self.yield_data():
+            print("pupil get :", element)
+            return element
+
+
+    def yield_data(self):
+        while self.person_q.isEmpty() is False :
+            time.sleep(3)
+            print("yield_data")
+            element = self.person_q.get()
+            yield element
+
+
+class CheatingSystem:
+
+    def __init__(self):
+        self.pupil_q = RedisQueue(name='pupil')
+        self.sound_q = RedisQueue(name='sound')
+        self.person_q = RedisQueue(name='person')
+
+    def create_queue(self, name):
+        return RedisQueue(name)
+
+    def update_queue(self, name : str):
+        pass
+
+    def typeToStr(self, type):
+        pass
+
+    def update(self, data : str, type : int) -> None : 
+        # print("how many data in pupil_queue f:", self.pupil_q.size())
+        # print("how many data in sound_queue f:", self.sound_q.size())
+        # print(self.sound_q.isEmpty()) # False
+        # print(self.pupil_q.isEmpty()) # False
+
+        # 시간 때문인건가
+        if type == 0:
+            self.pupil_q.put(data)
         
+        if type == 1:
+            self.sound_q.put(data)
 
+    def pupil_get(self):
+        for element in self.yield_data():
+            print("pupil get :", element)
+            return element
 
-#class personRecognition()
+    def yield_data(self):
+
+        while self.pupil_q.isEmpty() is False :
+            print(self.pupil_q.size())
+            time.sleep(5)
+            element = self.pupil_q.get()
+            yield element
+
